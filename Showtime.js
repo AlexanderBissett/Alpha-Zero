5d5ff185a05909597f92761e63a33661c21b7b6d
AlexanderBissett <151410349+AlexanderBissett@users.noreply.github.com>
2024-09-03 01:49:35 +0200

    5

--- Showtime.js
+++ Showtime.js
@@ -5,12 +5,17 @@ function runCommand(command) {
   exec(`start cmd /k ${command}`);
 }
 
-// Run the Scanner
+// Run the Scanner bot
 runCommand('node C:\\Users\\Alexander\\AlphaZero\\js\\Scanner\\Alpha.js');
 
-// Wait 5 seconds before running the B·R·A·I·N
+// Wait 5 seconds before running the B·R·A·I·N bot
 setTimeout(() => {
   runCommand('node C:\\Users\\Alexander\\AlphaZero\\js\\Scanner\\Brain.mjs');
+}, 5000);
+
+// Wait 5 seconds before running the Balancer bot
+setTimeout(() => {
+  runCommand('node C:\\Users\\Alexander\\AlphaZero\\js\\Scanner\\Balancer.js');
 }, 5000);
 
 // Wait 5 seconds before running the Gambler bot
