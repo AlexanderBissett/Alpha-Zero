1d99e63618eba200b12f52705d7f5b5be0f5b1a6
AlexanderBissett <151410349+AlexanderBissett@users.noreply.github.com>
2024-10-20 00:08:56 +0200

    alpha release 0.2.8.5

--- bot/Initializer.js
+++ bot/Initializer.js
@@ -47,4 +47,4 @@ fs.watch(scanlogDir, (eventType, filename) => {
   }
 });
 
-//Alpha-Zero Fundamental Model 0.2.8 by 101 @ The Organitation
+//Alpha-Zero Fundamental Model 0.2.8.5 by 101 @ The Organitation
